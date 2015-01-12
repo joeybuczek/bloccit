18abcacab96f3189977f349cdce552dcbe0be89b
Joey Buczek <joe.buczek@gmail.com>
2015-01-12 04:53:42 +0000

    Alternate partial usage with css classes to display errors

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,6 +3,14 @@ module ApplicationHelper
     "JoeyB"
   end
   
+  # Set CSS class for error visuals on form element if any occur
+  def error_check_tag(errors)
+    if errors.any?
+      'has-error'
+    end
+  end
+  
+  # Leaving the following method for future reference with passing a block
   def form_group_tag(errors, &block)
     if errors.any?
       content_tag :div, capture(&block), class: 'form-group has-error'
