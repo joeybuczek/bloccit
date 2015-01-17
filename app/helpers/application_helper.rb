6049722a5d315b07a57f2cde1ce7d112459be0db
Joey Buczek <joe.buczek@gmail.com>
2015-01-17 03:11:49 +0000

    Finalize changes with alt-markdown

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -10,21 +10,21 @@ module ApplicationHelper
     end
   end
   
-  # Leaving the following method for future reference with passing a block
-  def form_group_tag(errors, &block)
-    if errors.any?
-      content_tag :div, capture(&block), class: 'form-group has-error'
-    else
-      content_tag :div, capture(&block), class: 'form-group'
-    end
-  end
+#   # Leaving the following method for future reference with passing a block
+#   def form_group_tag(errors, &block)
+#     if errors.any?
+#       content_tag :div, capture(&block), class: 'form-group has-error'
+#     else
+#       content_tag :div, capture(&block), class: 'form-group'
+#     end
+#   end
   
-  # Markdown helper
-  def markdown(text)
-    renderer = Redcarpet::Render::HTML.new
-    extensions = {fenced_code_blocks: true}
-    redcarpet = Redcarpet::Markdown.new(renderer, extensions)
-    (redcarpet.render text).html_safe
-  end
+#   # Markdown helper
+#   def markdown(text)
+#     renderer = Redcarpet::Render::HTML.new
+#     extensions = {fenced_code_blocks: true}
+#     redcarpet = Redcarpet::Markdown.new(renderer, extensions)
+#     (redcarpet.render text).html_safe
+#   end
   
 end
