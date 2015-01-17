3a5c840d513e84d405560eeb8d477c67a8441018
Joey Buczek <joe.buczek@gmail.com>
2015-01-17 02:38:41 +0000

    More testing on pagination

--- lib/paginate.rb
+++ lib/paginate.rb
@@ -6,20 +6,23 @@ module Paginate
   def paginate(hash={})
     
     # pass these values to variables via 'hash[:key]'
-    per_page = hash[:per_page]
-    page = hash[:page]
+    per_page = hash[:per_page].to_i
+    page = hash[:page].to_i || 1
     
-    # calculate total number of pages: count / per_page, rounded up
-    total_pages = (count.to_f / per_page.to_f).ceil
+    limit(per_page).offset((page - 1) * per_page)
+
     
-    # create the collections for each page (via limit/offset methods) and store them in an array
-    array = []
-    for page_num in 0..(total_pages - 1) do
-      array << limit(per_page).offset(page_num * per_page)
-    end
+#     # calculate total number of pages: count / per_page, rounded up
+#     total_pages = (count.to_f / per_page.to_f).ceil
     
-    # create return value for this method
-    { items: array, pages: total_pages }
+#     # create the collections for each page (via limit/offset methods) and store them in an array
+#     array = []
+#     for page_num in 0..(total_pages - 1) do
+#       array << limit(per_page).offset(page_num * per_page)
+#     end
+    
+#     # create return value for this method
+#     { items: array, pages: total_pages }
     
   end
   
