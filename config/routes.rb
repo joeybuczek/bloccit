875131839e826d2eba93bd8f2a7b088b73c03d39
Joey Buczek <joe.buczek@gmail.com>
2015-01-21 02:34:16 +0000

    Added commenting - still need to make validation errors useful

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,16 @@
 Rails.application.routes.draw do
 
+  #   get 'comments/new'
+
   #   set routing for users controller
   devise_for :users
   resources :users, only: [:update]
   
-  #   set routing for post controller 
+  #   set routing for topic, post, and comment controllers
   resources :topics do
-    resources :posts, except: [:index]
+    resources :posts, except: [:index] do
+      resources :comments, only: [:create]
+    end
   end
 
   #   about route
